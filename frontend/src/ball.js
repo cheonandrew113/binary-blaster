10454cb1053713339cb5020999c9c5c932f58f98
Tyler Knapp <tgknapp11@gmail.com>
2019-09-09 17:27:44 -0400

    Luis's animations

--- frontend/src/ball.js
+++ frontend/src/ball.js
@@ -1,6 +1,7 @@
 // document.addEventListener("DOMContentLoaded", () => {
-//   const bg = document.getElementById("game-screen");
-//   let target = `<div style="position: relative;"><link style="position: relative;" href="#" class="target-link"><img style="height: 75px; width: auto;" src="http://plug.nyc/wp-content/uploads/2019/09/target.png" class="target" alt=""></link href="#"></div>`;
+//   const gameContainer = document.getElementsByClassName("scene");
 
-//   bg.insertAdjacentHTML("afterbegin", target);
+//   gameContainer.addEventListener("click", function(e) {
+//     console.log("hello");
+//   });
 // });
